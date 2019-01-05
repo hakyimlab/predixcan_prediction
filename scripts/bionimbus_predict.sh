a40fd89d19bae261bbb319d19b08d1ccef67513e
Milton Pividori <miltondp@uchicago.edu>
2019-01-04 18:05:41 -0600

    script to run in all tissues

--- scripts/bionimbus_predict.sh
+++ scripts/bionimbus_predict.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-# example for whole blood
+mkdir -p prediction_output
 
-python predict.py \
+parallel -j3 \
+'python predict.py \
   --bgens-dir /mnt/ukb_v3/imp/ \
+  --bgens-prefix ukb_imp_chr \
   --bgens-sample-file /mnt/ukb_v3/link_files/ukb19526_imp_chr1_v3_s487395.sample \
-  --weights-file /mnt/software/gtex_v8_models/gtex_v8_Whole_Blood_itm_signif.db \
-  --output-file whole_blood.h5 \
-  --bgens-n-cache 1000                                     
+  --weights-file {} \
+  --output-file prediction_output/{/.}.h5 \
+  --bgens-n-cache 500 \
+  --bgens-writing-cache-size 500' \
+  ::: /mnt/software/gtex_v8_models/*.db
 
